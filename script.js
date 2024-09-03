34fa2f7beb8a07e4866f68f581df6aff255bac3f
maxi-linuxlite <jaimezmaximo@gmail>
2024-09-03 09:19:56 -0300

    add the function to get a random choice
    this function is the core of the program because the computer needs choose randomly.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+
+let string = "";
+function getComputerChoice(){
+    let num = Math.floor(Math.random() * 3)
+
+    if(num == 0){
+        string = "rock"
+    }
+    else if(num ==1){
+        string= "paper";
+    }
+    else{
+        string="scissor";
+    }
+    return string;
+}
+
+
+console.log(getComputerChoice());
